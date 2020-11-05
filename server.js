0308c5a52e1993bccff1cd0083772aa59a0d88ac
Leref <Juansantana75@icloud.com>
2020-11-05 11:49:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ prefix: "!"
  
 bot.onMessage()
  
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+const command = require(`./commands/${files}/${commands}`) 
 bot.command({
-name: "ping", 
-code: `Pong! \`$ping\`` 
+name: command.name,
+code: command.code
 })
+} 
+}
