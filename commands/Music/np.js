040b8c76d780538622fc2e4d63c2585b3e3f26b8
Leref <juansantana75@icloud.com>
2020-11-05 23:22:05 -0500

    Music Stuff

---
+++ commands/Music/np.js
@@ -0,0 +1,6 @@
+module.exports = ({
+    name: "np",
+    code: `
+    $description[Now im playing [$songInfo[title]]($songInfo[url]) - Added By: $username[$songInfo[userID]] - Duration: $songInfo[duration]]
+    `
+    });
---
+++ commands/Music/play.js
@@ -0,0 +1,7 @@
+module.exports = ({
+    name: "play",
+    code: `
+    $message has been added to the queue!
+    $playSong[$message;Something went wrong, Try Again !]
+    `
+    });
